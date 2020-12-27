288b551dd3d242529da3bb78524ab8d5e6c38666
Leong Hui Wong <wong.leonghui@gmail.com>
2020-12-27 20:50:13 +0800

    Refactor validation into data classes

--- amazon_feed_data.py
+++ amazon_feed_data.py
@@ -1,30 +1,84 @@
 from dataclasses import dataclass
 
 
+def string_to_boolean(string):
+    return string.lower().strip() in ['yes', 'true']
+
+
 @dataclass
-class _PriceFilter:
+class QueryStatus():
+    ok: bool
+    errors: list[str]
+
+
+@dataclass
+class _BaseQuery():
+    status: QueryStatus
+    query: str = None
+    country: str = 'US'
+
+    def validateCountry(self):
+        if self.country:
+            if not self.country.isalpha() or len(self.country) != 2:
+                self.status.errors.append('Invalid country code')
+            self.country = self.country.upper()
+
+
+@dataclass
+class _PriceFilter():
     min_price: str = None
     max_price: str = None
 
 
 @dataclass
-class _BaseQuery:
-    query: str
-    country: str = 'US'
+class _BaseQueryWithPriceFilter(_PriceFilter, _BaseQuery):
+    def validatePriceFilters(self):
+        if self.max_price and not self.max_price.isnumeric():
+            self.status.errors.append('Invalid max price')
+
+        if self.min_price and not self.min_price.isnumeric():
+            self.status.errors.append('Invalid min price')
 
 
 @dataclass
 class _AmazonSearchFilter:
-    strict: bool = False
     buybox_only: bool = False
+    strict: bool = False
+
+    def validateAmazonSearchFilters(self):
+        if self.buybox_only:
+            self.buybox_only = string_to_boolean(self.buybox_only)
+        if self.strict:
+            self.strict = string_to_boolean(self.strict)
 
 
 @dataclass
-class AmazonSearchQuery(_PriceFilter, _AmazonSearchFilter, _BaseQuery):
-    __slots__ = ['query', 'country', 'strict',
-                 'buybox_only', 'min_price', 'max_price']
+class AmazonSearchQuery(_AmazonSearchFilter, _BaseQueryWithPriceFilter):
+    __slots__ = ['query', 'country', 'min_price',
+                 'max_price', 'buybox_only', 'strict']
+
+    def __post_init__(self):
+        self.validateCountry()
+        self.validatePriceFilters()
+        self.validateAmazonSearchFilters()
+
+        if not isinstance(self.query, str):
+            self.status.errors.append('Invalid query')
+
+        if self.status.errors:
+            self.status.ok = False
 
 
 @dataclass
-class AmazonListQuery(_PriceFilter, _BaseQuery):
+class AmazonListQuery(_BaseQueryWithPriceFilter):
     __slots__ = ['query', 'country', 'min_price', 'max_price']
+
+    def __post_init__(self):
+        super().validateCountry()
+        super().validatePriceFilters()
+
+        if not isinstance(self.query, str):
+            self.status.errors.append('Invalid ASIN')
+
+        if self.status.errors:
+            self.status.ok = False
--- server.py
+++ server.py
@@ -2,84 +2,55 @@ from flask import Flask, request, jsonify, abort
 from flask.logging import create_logger
 
 from amazon_feed import get_search_results, get_item_listing
-from amazon_feed_data import AmazonSearchQuery, AmazonListQuery
+from amazon_feed_data import AmazonSearchQuery, AmazonListQuery, QueryStatus
 
 
 app = Flask(__name__)
 logger = create_logger(app)
 
 
-def string_to_boolean(string):
-    return string.lower().strip() in ['yes', 'true']
-
-
 def process_query():
     query_text = request.args.get('query')
+    country = request.args.get('country')
     min_price = request.args.get('min_price')
     max_price = request.args.get('max_price')
-    country_text = request.args.get('country')
     buybox_only_text = request.args.get('buybox_only')
     strict_text = request.args.get('strict')
 
-    if not isinstance(query_text, str):
-        abort(400, description='Please provide a valid query string.')
+    search_query = AmazonSearchQuery(QueryStatus(ok=True, errors=[]),
+                                     query_text, country, min_price, max_price, buybox_only_text, strict_text)
 
-    if min_price and not min_price.isnumeric():
-        abort(400, description='Invalid min price.')
-
-    if max_price and not max_price.isnumeric():
-        abort(400, description='Invalid max price.')
-
-    country = country_text.upper() if isinstance(
-        country_text, str) and len(country_text) == 2 else 'US'
-
-    buybox_only = True if isinstance(
-        buybox_only_text, str) and string_to_boolean(buybox_only_text) else False
-
-    strict = True if isinstance(
-        strict_text, str) and string_to_boolean(strict_text) else False
-
-    search_query = AmazonSearchQuery(
-        query_text, country, buybox_only, strict, min_price, max_price)
+    if not search_query.status.ok:
+        abort(400, description='Errors found: ' +
+              ', '.join(search_query.status.errors))
 
     logger.debug(search_query)  # log values
 
-    try:
-        output = get_search_results(search_query, logger)
-        response = jsonify(output)
-        response.mimetype = 'application/feed+json'
-        return response
-    except Exception as e:
-        abort(500, description=f"Error generating output: {e}")
+    output = get_search_results(search_query, logger)
+    response = jsonify(output)
+    response.mimetype = 'application/feed+json'
+    return response
 
 
 def process_listing():
-    id = request.args.get('id')
-    max_price = request.args.get('max_price')
+    query = request.args.get('id')
+    country = request.args.get('country')
     min_price = None
-    country_text = request.args.get('country')
+    max_price = request.args.get('max_price')
 
-    if not isinstance(id, str):
-        abort(400, description='Please provide a valid id.')
-
-    if max_price and not max_price.isnumeric():
-        abort(400, description='Invalid max price.')
-
-    country = country_text.upper() if isinstance(
-        country_text, str) and len(country_text) == 2 else 'US'
-
-    list_query = AmazonListQuery(
-        id, country, min_price, max_price)
+    list_query = AmazonListQuery(QueryStatus(ok=True, errors=[]),
+                                 query, country, min_price, max_price)
 
     logger.debug(list_query)  # log values
 
-    try:
-        output = get_item_listing(list_query, logger)
-        response = jsonify(output)
-        response.mimetype = 'application/feed+json'
-        return response
-    except Exception as e:
-        abort(500, description=f"Error generating output: {e}")
+    if not list_query.status.ok:
+        abort(400, description='Errors found: ' +
+              ', '.join(list_query.status.errors))
+
+    output = get_item_listing(list_query, logger)
+    response = jsonify(output)
+    response.mimetype = 'application/feed+json'
+    return response
 
 
 @app.route('/', methods=['GET'])
